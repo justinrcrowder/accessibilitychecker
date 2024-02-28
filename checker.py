adfc91b5c53954ba053926322bb697af21d516ee
Justin Crowder <jrcrowder@wisc.edu>
2024-02-28 09:46:13 -0600

    remove old logic from project

--- checker.py
+++
@@ -1,9 +0,0 @@
-import requests
-from secret import key
-
-webisteURL = 'https://www.google.com'
-url = 'https://wave.webaim.org/api/request?key=' + key + '&url=' + webisteURL
-
-r = requests.get(url)
-
-print(r.text)
